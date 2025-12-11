9984f8913055f33c553c2c21f8df2977502bb593
Toly Pochkin <mr.toly@gmail.com>
2025-12-11 17:31:28 -0500

    prepare for the release

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KmperTraceRuntime",
-            url: "https://github.com/mobiletoly/kmpertrace/releases/download/v0.1.10/KmperTraceRuntime.xcframework.zip",
-            checksum: "0fe1efc170d312c1f0141c328e4e0aeb5d0504980bdaaccce2cb2de2a051788c"
+            url: "https://github.com/mobiletoly/kmpertrace/releases/download/v0.1.11/KmperTraceRuntime.xcframework.zip",
+            checksum: "0a7ae407206ad6ffebf87de134fa17cf9b8bb38dbffc7c36cb94c040dc422470"
         )
     ]
 )
