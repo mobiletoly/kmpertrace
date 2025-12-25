74789c91d1caf4888d221f119217f66cadd477e8
Toly Pochkin <mr.toly@gmail.com>
2025-12-25 12:49:49 -0500

    chore: update SPM checksum for 0.3.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "KmperTraceRuntime",
             url: "https://github.com/mobiletoly/kmpertrace/releases/download/v0.3.1/KmperTraceRuntime.xcframework.zip",
-            checksum: "ab1f645650e10c79b48a2b5286aeb492deaebd2d739d5c3bf7c74ab640a6a1cf"
+            checksum: "2152c0f6756b74719c16a2ba7423172f4af7744ce53a5e95e127cbde1046c7f6"
         )
     ]
 )
